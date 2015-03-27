d62afff02bcb959c926fd932f5a1aa52b70b1aaf
Johnnathanalmeida <johnnathanalmeida@gmail.com>
2015-03-27 11:07:43 -0300

    Corrigindo bug no github e class training

---
+++ PartiuAcademia/PartiuAcademia.Core/Entities/Training.cs
@@ -0,0 +1,27 @@
+﻿using PartiuAcademia.Core.Entities.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartiuAcademia.Core.Entities
+{
+    [Table("tbTraining")]
+    public class Training : EntityBase
+    {
+        public User Teacher { get; set; }
+
+        public Objetivo Objetivo { get; set; }
+
+        public string Observation { get; set; }
+
+        public int Duration { get; set; }
+
+        public bool Status { get; set; }
+
+        public IList<TrainingRecord> lTrainingRecord { get; set; }
+
+    }
+}
